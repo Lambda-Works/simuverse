da275c01f85fa05842a66441e5699f3eb291f358
N1C0-P4Z <rubenpaz2105@gmail.com>
2026-07-05 04:04:06 -0300

    sidebar completo

--- apps/web/app/(authenticated)/admin/page.tsx
+++ apps/web/app/(authenticated)/admin/page.tsx
@@ -1,10 +1,5 @@
 'use client'
-import { AdminProvider } from '@/lib/admin-context'
 import AdminPanel from '@/views/AdminPanel'
 export default function AdminPage() {
-  return (
-    <AdminProvider>
-      <AdminPanel />
-    </AdminProvider>
-  )
+  return <AdminPanel />
 }
